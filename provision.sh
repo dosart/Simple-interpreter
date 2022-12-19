34538191c1de3d0913e1555d76a9419b7bcfeaac
dosart <Dysprosium92@ya.ru>
2022-12-19 21:21:30 +0300

    add black - code formatter

--- provision.sh
+++ provision.sh
@@ -1,36 +1,35 @@
-#!/usr/bin/env bash
+ #!/usr/bin/env bash
 
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
 
-echo "Installing python 3.9.0."
-cd /opt
-sudo wget https://www.python.org/ftp/python/3.9.0/Python-3.9.0.tgz
-
-sudo tar xzf Python-3.9.0.tgz
-cd Python-3.9.0
-sudo ./configure --enable-optimizations
-sudo make altinstall
-python3.9 --version
-cd /vagrant
-
-echo "Installing curl."
-sudo apt install -y curl
-curl -V
-
-echo "Installing poetry."
-curl -sSL https://install.python-poetry.org | python3 -
-
-echo "Adding poetry to PATH."
+echo "Loading terminal prompt formatting from .bash_prompt."
 echo >> ~/.bashrc
-echp 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc
-poetry --version
+echo "# load terminal prompt formatting from .bash_prompt" >> ~/.bashrc
+echo "source ~/.bash_prompt" >> ~/.bashrc
+sudo ln -s /vagrant/.bash_prompt ~/.bash_prompt
 
 echo "Automatically cd to /vagrant folder when vagrant ssh."
 echo >> ~/.bashrc
 echo "# automatically cd to /vagrant folder when vagrant ssh" >> ~/.bashrc
 echo "cd /vagrant" >> ~/.bashrc
 
-echo "Installing project dependencies."
+echo "Installing curl."
+sudo apt install -y curl
+
+echo "Installing poetry."
+curl -sSL https://install.python-poetry.org | python3 -
+
+echo "Adding poetry to PATH."
+export PATH="/home/vagrant/.local/bin:$PATH"
+echo >> ~/.bashrc
+echo 'export PATH="/home/vagrant/.local/bin:$PATH' >> ~/.bashrc
+source ~/.bashrc
+
+echo "Installing project dependencies and activating the virtual environment."
+cd /vagrant
+poetry config virtualenvs.in-project true
+poetry env use python3.8
 poetry install
+poetry shell
