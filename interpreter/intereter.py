ba58ae3ee7c1325a0fdc3fbf174e96b414a20841
dosart <Dysprosium92@ya.ru>
2023-01-04 23:34:41 +0300

    build: simple arithmetic interpreter (only + or - and integer)

--- interpreter/intereter.py
+++ interpreter/intereter.py
@@ -2,6 +2,7 @@
 
 """Interpreter's implementation."""
 
+from itertools import takewhile
 
 from interpreter.token import TokenType
 
@@ -15,4 +16,20 @@ def is_token_plus_or_minus(token):
     Returns:
         bool: true if the token is equal to plus or minus
     """
-    return token.type in {TokenType.plus, token.type == TokenType.minus}
+    return token.type in {TokenType.plus, TokenType.minus}
+
+
+def apply(tokens):
+    """Calculate simple arithmetic expressions (+ and -sign only).
+
+    Args:
+        tokens: the expression consists of tokens
+
+    Returns:
+        int: the result of calculating the expression
+    """
+    result = next(tokens)
+    for sign in takewhile(is_token_plus_or_minus, tokens):
+        right = next(tokens)
+        result.value = sign.value(result.value, right.value)
+    return result
--- tests/test_interpreter.py
+++ tests/test_interpreter.py
@@ -1,9 +1,10 @@
 # -*- coding:utf-8 -*-
 
-"""Lexer representation tests."""
+"""Interpreter representation tests."""
 
 import pytest
-from interpreter.intereter import is_token_plus_or_minus
+from interpreter.intereter import apply, is_token_plus_or_minus
+from interpreter.lexer import get_tokens
 from interpreter.token import Token, TokenType
 
 
@@ -33,3 +34,108 @@ def test_is_token_plus_or_minus2(test_input):
     """
     token = Token(test_input, 0)
     assert is_token_plus_or_minus(token) is True
+
+
+@pytest.mark.parametrize(
+    "test_input",
+    ["10+11", "10 + 11 ", " 10 + 11 "],
+)
+def test_apply1(test_input):
+    """Check a simple expression.
+
+    Args:
+        test_input: data for test
+    """
+    tokens = get_tokens(test_input)
+    result = apply(tokens)
+    assert result.value == 21
+
+
+@pytest.mark.parametrize(
+    "test_input",
+    ["10+11+11", "10 + 11 + 11 ", " 10 + 11   + 11"],
+)
+def test_apply2(test_input):
+    """Check a simple expression.
+
+    Args:
+        test_input: data for test
+    """
+    tokens = get_tokens(test_input)
+    result = apply(tokens)
+    assert result.value == 32
+
+
+@pytest.mark.parametrize(
+    "test_input",
+    ["10", "10 ", " 10 "],
+)
+def test_apply3(test_input):
+    """Check a simple expression.
+
+    Args:
+        test_input: data for test
+    """
+    tokens = get_tokens(test_input)
+    result = apply(tokens)
+    assert result.value == 10
+
+
+@pytest.mark.parametrize(
+    "test_input",
+    ["10-5", "10 - 5 ", " 10 - 5   "],
+)
+def test_apply4(test_input):
+    """Check a simple expression.
+
+    Args:
+        test_input: data for test
+    """
+    tokens = get_tokens(test_input)
+    result = apply(tokens)
+    assert result.value == 5
+
+
+@pytest.mark.parametrize(
+    "test_input",
+    ["10-5-2", "10 - 5 -2 ", " 10 - 5 - 2  "],
+)
+def test_apply5(test_input):
+    """Check a simple expression.
+
+    Args:
+        test_input: data for test
+    """
+    tokens = get_tokens(test_input)
+    result = apply(tokens)
+    assert result.value == 3
+
+
+@pytest.mark.parametrize(
+    "test_input",
+    ["10-5+2", "10 - 5 +2 ", " 10 - 5 + 2  "],
+)
+def test_apply6(test_input):
+    """Check a simple expression.
+
+    Args:
+        test_input: data for test
+    """
+    tokens = get_tokens(test_input)
+    result = apply(tokens)
+    assert result.value == 7
+
+
+@pytest.mark.parametrize(
+    "test_input",
+    ["10-2-5+2", "10 -2 - 5 +2 ", " 10-2 - 5 + 2  "],
+)
+def test_apply7(test_input):
+    """Check a simple expression.
+
+    Args:
+        test_input: data for test
+    """
+    tokens = get_tokens(test_input)
+    result = apply(tokens)
+    assert result.value == 5
