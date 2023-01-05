c016bfc896347ff0d53d84c7e0723102808c4431
dosart <Dysprosium92@ya.ru>
2023-01-05 23:45:55 +0300

    build: checking the correctness of the expression

--- interpreter/intereter.py
+++ interpreter/intereter.py
@@ -4,7 +4,8 @@
 
 from itertools import takewhile
 
-from interpreter.token import TokenType
+from interpreter.either import make_error_message, make_left, make_right
+from interpreter.token import TokenType, make_integer
 
 
 def is_token_plus_or_minus(token):
@@ -19,6 +20,76 @@ def is_token_plus_or_minus(token):
     return token.type in {TokenType.plus, TokenType.minus}
 
 
+def try_get_integer(iterator):
+    """Return the current taken from the iterator to the monad either.
+
+    Args:
+        iterator: iterator containing tokens
+
+    Returns:
+        either: monad contains token. if the type is not an integer return left
+    """
+    try:
+        return get_token(iterator, {TokenType.integer})
+    except StopIteration:
+        return make_left(make_error_message("value isn't integer"))
+
+
+def try_get_sign(iterator):
+    """Return the current taken from the iterator to the monad either.
+
+    Args:
+        iterator: iterator containing tokens
+
+    Returns:
+        either: monad contains token. if the type is not a sign return left
+    """
+    try:
+        return get_token(iterator, {TokenType.plus, TokenType.minus})
+    except StopIteration:
+        return make_left(make_error_message("value isn't sign"))
+
+
+def get_token(iterator, token_types):
+    """Return the current token from the iterator. Check the current token type.
+
+    Args:
+        iterator: iterator containing tokens
+        token_types(set): if the type is not a token_types return left
+
+    Returns:
+        either: monad contains tokens
+    """
+    maybe_integer = next(iterator)
+    if maybe_integer.type in token_types:
+        return make_right(maybe_integer)
+    return make_left("value isn't correct")
+
+
+def bind(func, arg1, arg2):
+    """Return the result of func(arg1, arg2).
+
+    Args:
+        func: function
+        arg1: argument of a function in either monad
+        arg2: argument of a function in either monad
+
+    Returns:
+        either: the monad contains the result tokens func(arg1, arg2)
+    """
+    if arg1.is_left:
+        return arg1
+    if arg2.is_left:
+        return arg2
+
+    one = arg1.get_right()
+    two = arg2.get_right()
+
+    result = make_integer(0)
+    result.value = func.value(one.value, two.value)
+    return make_right(result)
+
+
 def apply(tokens):
     """Calculate simple arithmetic expressions (+ and -sign only).
 
@@ -28,8 +99,8 @@ def apply(tokens):
     Returns:
         int: the result of calculating the expression
     """
-    result = next(tokens)
+    result = try_get_integer(tokens)
     for sign in takewhile(is_token_plus_or_minus, tokens):
-        right = next(tokens)
-        result.value = sign.value(result.value, right.value)
+        right = try_get_integer(tokens)
+        result = bind(sign, result, right)
     return result
--- tests/test_interpreter.py
+++ tests/test_interpreter.py
@@ -2,10 +2,13 @@
 
 """Interpreter representation tests."""
 
+from itertools import permutations
+
 import pytest
-from interpreter.intereter import apply, is_token_plus_or_minus
+from interpreter.either import make_left, make_right
+from interpreter.intereter import apply, bind, is_token_plus_or_minus, try_get_integer
 from interpreter.lexer import get_tokens
-from interpreter.token import Token, TokenType
+from interpreter.token import Token, TokenType, make_eof, make_integer, make_sign
 
 
 @pytest.mark.parametrize(
@@ -48,7 +51,7 @@ def test_apply1(test_input):
     """
     tokens = get_tokens(test_input)
     result = apply(tokens)
-    assert result.value == 21
+    assert result.get_right().value == 21
 
 
 @pytest.mark.parametrize(
@@ -63,7 +66,7 @@ def test_apply2(test_input):
     """
     tokens = get_tokens(test_input)
     result = apply(tokens)
-    assert result.value == 32
+    assert result.get_right().value == 32
 
 
 @pytest.mark.parametrize(
@@ -78,7 +81,7 @@ def test_apply3(test_input):
     """
     tokens = get_tokens(test_input)
     result = apply(tokens)
-    assert result.value == 10
+    assert result.get_right().value == 10
 
 
 @pytest.mark.parametrize(
@@ -93,7 +96,7 @@ def test_apply4(test_input):
     """
     tokens = get_tokens(test_input)
     result = apply(tokens)
-    assert result.value == 5
+    assert result.get_right().value == 5
 
 
 @pytest.mark.parametrize(
@@ -108,7 +111,7 @@ def test_apply5(test_input):
     """
     tokens = get_tokens(test_input)
     result = apply(tokens)
-    assert result.value == 3
+    assert result.get_right().value == 3
 
 
 @pytest.mark.parametrize(
@@ -123,7 +126,7 @@ def test_apply6(test_input):
     """
     tokens = get_tokens(test_input)
     result = apply(tokens)
-    assert result.value == 7
+    assert result.get_right().value == 7
 
 
 @pytest.mark.parametrize(
@@ -138,4 +141,65 @@ def test_apply7(test_input):
     """
     tokens = get_tokens(test_input)
     result = apply(tokens)
-    assert result.value == 5
+    assert result.get_right().value == 5
+
+
+def test_get_integer_true():
+    """Check a simple expression."""
+    integers = [make_integer(10), make_integer(20)]
+    either_integer = try_get_integer(iter(integers))
+    assert either_integer.is_right is True
+    assert either_integer.get_right().type == TokenType.integer
+    assert either_integer.get_right().value == 10
+
+
+def test_get_integer_false():
+    """Check a simple expression."""
+    integers = [
+        make_sign("+"),
+        make_sign("-"),
+        make_sign("*"),
+        make_sign("/"),
+        make_eof(),
+    ]
+    iterator = iter(integers)
+    count = 0
+    while count < len(integers):
+        either_integer = try_get_integer(iterator)
+        assert either_integer.is_right is False
+        assert either_integer.is_left is True
+
+        count += 1
+
+
+def test_get_integer_empty_list():
+    """Check a simple expression."""
+    empty = iter([])
+    either_integer = try_get_integer(empty)
+    assert either_integer.is_right is False
+    assert either_integer.is_left is True
+
+
+def test_bind_good():
+    """Check a simple expression."""
+    sign = make_sign("+")
+    arg1 = make_right(make_integer(10))
+    arg2 = make_right(make_integer(20))
+
+    result = bind(func=sign, arg1=arg1, arg2=arg2)
+
+    assert result.is_right is True
+    assert result.get_right().value == 30
+
+
+def test_bind_bad():
+    """Check a simple expression."""
+    tokens = (
+        make_left(make_integer(10)),
+        make_right(make_integer(20)),
+    )
+
+    func = (make_sign("+"),)
+    for arg1, arg2 in permutations(tokens):
+        result = bind(func, arg1, arg2)
+        assert result.is_left is True
