70e9baf78d82a17bfe5b52ef216e2395803a22ac
dosart <Dysprosium92@ya.ru>
2023-03-03 21:59:10 +0300

    refactor: change the implementation of the interpreter

--- interpreter/intereter.py
+++ interpreter/intereter.py
@@ -2,142 +2,20 @@
 
 """Interpreter's implementation."""
 
-from itertools import takewhile
-
-from interpreter.either import make_error_message, make_left, make_right
-from interpreter.token import TokenType, make_integer
-
-
-def apply_plus_or_minus(tokens):
-    """Calculate simple arithmetic expressions (+ and - sign only).
-
-    Args:
-        tokens: the expression consists of tokens
-
-    Returns:
-        int: the result of calculating the expression
-    """
-    return apply(tokens, is_token_plus_or_minus)
+from interpreter.lexer import get_token
+from interpreter.parser import Parser
+from interpreter.visitor import visit
 
 
-def apply_multiply_or_divide(tokens):
-    """Calculate simple arithmetic expressions (* and / sign only).
+def interpret(text):
+    """Execute programm.
 
     Args:
-        tokens: the expression consists of tokens
+        text: program text
 
     Returns:
-        int: the result of calculating the expression
+        result: calculation result
     """
-    return apply(tokens, is_token_multiply_or_divide)
-
-
-def is_token_plus_or_minus(token):
-    """Return true if the token is equal to plus or minus.
-
-    Args:
-        token: token for verification
-
-    Returns:
-        bool: true if the token is equal to plus or minus
-    """
-    return token.type in {TokenType.plus, TokenType.minus}
-
-
-def is_token_multiply_or_divide(token):
-    """Return true if the token is equal to multiply or divide.
-
-    Args:
-        token: token for verification
-
-    Returns:
-        bool: true if the token is equal to multiply or divide
-    """
-    return token.type in {TokenType.multiply, TokenType.divide}
-
-
-def apply(tokens, predicate):
-    """Calculate simple arithmetic expressions.
-
-    Args:
-        tokens: the expression consists of tokens
-        predicate: which tokens need to be returned
-
-    Returns:
-        int: the result of calculating the expression
-    """
-    result = try_get_integer(tokens)
-    for sign in takewhile(predicate, tokens):
-        right = try_get_integer(tokens)
-        result = bind(sign, result, right)
-    return result
-
-
-def try_get_integer(iterator):
-    """Return the current taken from the iterator to the monad either.
-
-    Args:
-        iterator: iterator containing tokens
-
-    Returns:
-        either: monad contains token. if the type is not an integer return left
-    """
-    try:
-        return get_token(iterator, {TokenType.integer})
-    except StopIteration:
-        return make_left(make_error_message("value isn't integer"))
-
-
-def try_get_sign(iterator):
-    """Return the current taken from the iterator to the monad either.
-
-    Args:
-        iterator: iterator containing tokens
-
-    Returns:
-        either: monad contains token. if the type is not a sign return left
-    """
-    try:
-        return get_token(iterator, {TokenType.plus, TokenType.minus})
-    except StopIteration:
-        return make_left(make_error_message("value isn't sign"))
-
-
-def get_token(iterator, token_types):
-    """Return the current token from the iterator. Check the current token type.
-
-    Args:
-        iterator: iterator containing tokens
-        token_types(set): if the type is not a token_types return left
-
-    Returns:
-        either: monad contains tokens
-    """
-    maybe_integer = next(iterator)
-    if maybe_integer.type in token_types:
-        return make_right(maybe_integer)
-    return make_left("value isn't correct")
-
-
-def bind(func, arg1, arg2):
-    """Return the result of func(arg1, arg2).
-
-    Args:
-        func: function
-        arg1: argument of a function in either monad
-        arg2: argument of a function in either monad
-
-    Returns:
-        either: the monad contains the result tokens func(arg1, arg2)
-    """
-    if arg1.is_left:
-        return arg1
-    if arg2.is_left:
-        return arg2
-
-    one = arg1.get_right()
-    two = arg2.get_right()
-
-    result = make_integer(0)
-    result.value = func.value(one.value, two.value)
-    return make_right(result)
+    parser = Parser(get_token(text))
+    ast = parser.parse()
+    return visit(ast)
--- tests/test_interpreter.py
+++ tests/test_interpreter.py
@@ -2,47 +2,9 @@
 
 """Interpreter representation tests."""
 
-from itertools import permutations
-
 import pytest
-from interpreter.either import make_left, make_right
-from interpreter.intereter import (
-    apply_multiply_or_divide,
-    apply_plus_or_minus,
-    bind,
-    is_token_plus_or_minus,
-    try_get_integer,
-)
-from interpreter.lexer import get_tokens
-from interpreter.token import Token, TokenType, make_eof, make_integer, make_sign
 
-
-@pytest.mark.parametrize(
-    "test_input",
-    [TokenType.integer, TokenType.eof, TokenType.multiply, TokenType.divide],
-)
-def test_is_token_plus_or_minus1(test_input):
-    """Check a simple expression.
-
-    Args:
-        test_input: data for test
-    """
-    token = Token(test_input, 0)
-    assert is_token_plus_or_minus(token) is False
-
-
-@pytest.mark.parametrize(
-    "test_input",
-    [TokenType.plus, TokenType.minus],
-)
-def test_is_token_plus_or_minus2(test_input):
-    """Check a simple expression.
-
-    Args:
-        test_input: data for test
-    """
-    token = Token(test_input, 0)
-    assert is_token_plus_or_minus(token) is True
+from interpreter.intereter import interpret
 
 
 @pytest.mark.parametrize(
@@ -55,9 +17,7 @@ def test_apply_plus_or_minus1(test_input):
     Args:
         test_input: data for test
     """
-    tokens = get_tokens(test_input)
-    result = apply_plus_or_minus(tokens)
-    assert result.get_right().value == 21
+    assert interpret(test_input) == 21
 
 
 @pytest.mark.parametrize(
@@ -70,9 +30,7 @@ def test_apply_plus_or_minus2(test_input):
     Args:
         test_input: data for test
     """
-    tokens = get_tokens(test_input)
-    result = apply_plus_or_minus(tokens)
-    assert result.get_right().value == 32
+    assert interpret(test_input) == 32
 
 
 @pytest.mark.parametrize(
@@ -85,9 +43,7 @@ def test_apply_plus_or_minus3(test_input):
     Args:
         test_input: data for test
     """
-    tokens = get_tokens(test_input)
-    result = apply_plus_or_minus(tokens)
-    assert result.get_right().value == 10
+    assert interpret(test_input) == 10
 
 
 @pytest.mark.parametrize(
@@ -100,9 +56,7 @@ def test_apply_plus_or_minus4(test_input):
     Args:
         test_input: data for test
     """
-    tokens = get_tokens(test_input)
-    result = apply_plus_or_minus(tokens)
-    assert result.get_right().value == 5
+    assert interpret(test_input) == 5
 
 
 @pytest.mark.parametrize(
@@ -115,9 +69,7 @@ def test_apply_plus_or_minus5(test_input):
     Args:
         test_input: data for test
     """
-    tokens = get_tokens(test_input)
-    result = apply_plus_or_minus(tokens)
-    assert result.get_right().value == 3
+    assert interpret(test_input) == 3
 
 
 @pytest.mark.parametrize(
@@ -130,9 +82,7 @@ def test_apply_plus_or_minus6(test_input):
     Args:
         test_input: data for test
     """
-    tokens = get_tokens(test_input)
-    result = apply_plus_or_minus(tokens)
-    assert result.get_right().value == 7
+    assert interpret(test_input) == 7
 
 
 @pytest.mark.parametrize(
@@ -145,142 +95,4 @@ def test_apply_plus_or_minus7(test_input):
     Args:
         test_input: data for test
     """
-    tokens = get_tokens(test_input)
-    result = apply_plus_or_minus(tokens)
-    assert result.get_right().value == 5
-
-
-def test_get_integer_true():
-    """Check a simple expression."""
-    integers = [make_integer(10), make_integer(20)]
-    either_integer = try_get_integer(iter(integers))
-    assert either_integer.is_right is True
-    assert either_integer.get_right().type == TokenType.integer
-    assert either_integer.get_right().value == 10
-
-
-def test_get_integer_false():
-    """Check a simple expression."""
-    integers = [
-        make_sign("+"),
-        make_sign("-"),
-        make_sign("*"),
-        make_sign("/"),
-        make_eof(),
-    ]
-    iterator = iter(integers)
-    count = 0
-    while count < len(integers):
-        either_integer = try_get_integer(iterator)
-        assert either_integer.is_right is False
-        assert either_integer.is_left is True
-
-        count += 1
-
-
-def test_get_integer_empty_list():
-    """Check a simple expression."""
-    empty = iter([])
-    either_integer = try_get_integer(empty)
-    assert either_integer.is_right is False
-    assert either_integer.is_left is True
-
-
-def test_bind_good():
-    """Check a simple expression."""
-    sign = make_sign("+")
-    arg1 = make_right(make_integer(10))
-    arg2 = make_right(make_integer(20))
-
-    result = bind(func=sign, arg1=arg1, arg2=arg2)
-
-    assert result.is_right is True
-    assert result.get_right().value == 30
-
-
-def test_bind_bad():
-    """Check a simple expression."""
-    tokens = (
-        make_left(make_integer(10)),
-        make_right(make_integer(20)),
-    )
-
-    func = (make_sign("+"),)
-    for arg1, arg2 in permutations(tokens):
-        result = bind(func, arg1, arg2)
-        assert result.is_left is True
-
-
-@pytest.mark.parametrize(
-    "test_input",
-    ["10-2-", "+10 -2", " 10 ++ 3  ", "-10++2++4"],
-)
-def test_apply_plus_or_minus_bad1(test_input):
-    """Check a simple expression.
-
-    Args:
-        test_input: data for test
-    """
-    tokens = get_tokens(test_input)
-    result = apply_plus_or_minus(tokens)
-    assert result.is_left is True
-
-
-@pytest.mark.parametrize(
-    "test_input",
-    ["10*11", "10 * 11 ", " 10 * 11 "],
-)
-def test_apply_multiply_or_divide1(test_input):
-    """Check a simple expression.
-
-    Args:
-        test_input: data for test
-    """
-    tokens = get_tokens(test_input)
-    result = apply_multiply_or_divide(tokens)
-    assert result.get_right().value == 110
-
-
-@pytest.mark.parametrize(
-    "test_input",
-    ["10/1", "10 / 1 ", " 10 / 1 "],
-)
-def test_apply_multiply_or_divide2(test_input):
-    """Check a simple expression.
-
-    Args:
-        test_input: data for test
-    """
-    tokens = get_tokens(test_input)
-    result = apply_multiply_or_divide(tokens)
-    assert result.get_right().value == 10
-
-
-@pytest.mark.parametrize(
-    "test_input",
-    ["10*2*5/2", "10 *2 * 5 /2 ", " 10*2 * 5 / 2  "],
-)
-def test_apply_multiply_or_divide3(test_input):
-    """Check a simple expression.
-
-    Args:
-        test_input: data for test
-    """
-    tokens = get_tokens(test_input)
-    result = apply_multiply_or_divide(tokens)
-    assert result.get_right().value == 50
-
-
-@pytest.mark.parametrize(
-    "test_input",
-    ["10*2*", "*10 /2", " 10 ** 3  ", "*10//2**4"],
-)
-def test_apply_multiply_or_divide_bad1(test_input):
-    """Check a simple expression.
-
-    Args:
-        test_input: data for test
-    """
-    tokens = get_tokens(test_input)
-    result = apply_multiply_or_divide(tokens)
-    assert result.is_left is True
+    assert interpret(test_input) == 5
