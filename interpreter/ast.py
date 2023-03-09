683590a71eb1bc5d0dbcf1e933e763f21618e3a0
dosart <Dysprosium92@ya.ru>
2023-03-09 19:01:05 +0300

    build: CompoundOperator, AssiginOperator, Variable, EmptyOperator classes

--- interpreter/ast.py
+++ interpreter/ast.py
@@ -84,3 +84,58 @@ class UnaryOperation(AST):
             operator=self.op,
             expression=self.right,
         )
+
+
+class CompoundOperator(AST):
+    """Represents a 'BEGIN ... END' block"""
+
+    def __init__(self):
+        self.children = []
+
+
+class AssiginOperator(AST):
+    """Implementation of assign operator."""
+
+    def __init__(self, left, op, right):
+        """Construct a new assign operator.
+
+        Args:
+            left: variable token
+            op: operator token
+            expr: expression to evaluate (BinaryOperation or UnaryOperation)
+        """
+        self.left = left
+        self.op = op
+        self.right = right
+
+    def __repr__(self):
+        """Represent object as a string.
+
+        Returns:
+            str: represent object as a string
+
+        """
+        return "AssiginOperator({variable}, {operator},  {expression})".format(
+            variable=self.left,
+            operator=self.op,
+            right_child=self.right,
+        )
+
+
+class Variable(AST):
+    """Implementation of variable."""
+
+    def __init__(self, token):
+        """Construct a new variable.
+
+        Args:
+            token: variable token(type is TokenType.variable)
+        """
+        self.token = token
+        self.value = token.value
+
+
+class EmptyOperator:
+    """Implementation of the AST base class."""
+
+    pass
